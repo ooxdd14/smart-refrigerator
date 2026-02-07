230c827019f6da8d4573adaa591f4d563744bbd3
ooxdd14 <hello3455l@gmail.com>
2026-02-07 17:19:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ if img_file:
     with st.spinner("n8n 서버로 안전하게 보내는 중..."):
         try:
             # 파일을 n8n이 받을 수 있는 형태로 변환
-           files = {"data": ("receipt.jpg", img_file.getvalue(), "image/jpeg")}         
-            response = requests.post(URL, files=files) # 위아래 줄 시작 위치가 같아야 함!
+           files = {"data": ("receipt.jpg", img_file.getvalue(), "image/jpeg")}
+            response = requests.post(URL, files=files)
             
             # n8n Webhook으로 전송
             response = requests.post(URL, files=files)
@@ -41,6 +41,7 @@ if img_file:
 
 st.divider()
 st.caption("Tip: 사진을 올린 후 n8n 화면에서 데이터가 들어오는지 새로고침하며 확인하세요.")
+
 
 
 
